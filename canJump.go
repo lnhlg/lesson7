259a2eaaf035b27fcf9639a82fb4fdfe4da252d1
lnhlg <jxlnhlg@gmail.com>
2022-02-22 21:32:18 +0800

    Update canJump.go

--- canJump.go
+++ canJump.go
@@ -1,5 +1,11 @@
 package lesson7
 
+//canJump: 跳跃游戏
+//给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。判断你是否能够到达最后一个下标。
+/*parameter
+nums: 非负整数数组
+return: 是否能够到达最后一个下标
+*/
 func canJump(nums []int) bool {
 
 	length := len(nums)
